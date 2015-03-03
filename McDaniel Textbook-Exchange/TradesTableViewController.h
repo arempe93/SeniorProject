01e64a53a712fc1cc930cbb77a94f6b2dacc5131
Andrew Rempe <andrewrempe@gmail.com>
2015-03-03 13:50:29 -0500

    Adds TradesTableViewController

---
+++ McDaniel Textbook-Exchange/TradesTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TradesTableViewController.h
+//  McDaniel Textbook-Exchange
+//
+//  Created by ajr009 on 3/3/15.
+//  Copyright (c) 2015 Rempire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TradesTableViewController : UITableViewController
+
+@end
