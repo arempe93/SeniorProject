50080eedff99d604c88fb5898e60ce3afe6c280f
Andrew Rempe <andrewrempe@gmail.com>
2015-03-03 13:46:12 -0500

    Adds WishlistTableViewController

---
+++ McDaniel Textbook-Exchange/WishlistTableViewController.m
@@ -0,0 +1,102 @@
+//
+//  WishlistTableViewController.m
+//  McDaniel Textbook-Exchange
+//
+//  Created by ajr009 on 3/3/15.
+//  Copyright (c) 2015 Rempire. All rights reserved.
+//
+
+#import "WishlistTableViewController.h"
+
+@interface WishlistTableViewController ()
+
+@property NSMutableArray *wishlist;
+@end
+
+@implementation WishlistTableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.wishlist = [[NSMutableArray alloc] init];
+    [self loadInitialData];
+}
+
+- (void)loadInitialData {
+    
+    [self.wishlist addObject:@"Velocity"];
+    [self.wishlist addObject:@"Fahrenheit 451"];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+
+    return [self.wishlist count];
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"wishlistCell" forIndexPath:indexPath];
+    
+    cell.textLabel.text = [self.wishlist objectAtIndex:indexPath.row];
+    
+    return cell;
+}
+
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
